3936830ff677b282b694b3e4662c23a1fe545747
VishalSingh-07 <vishalsinghagr25@gmail.com>
2023-01-20 21:24:29 +0530

    Adding [Add Two integers] Question

---
+++ Easy/Add_two_integers.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Time complexity --> O(1) and Space --> O(1)
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1+num2;
+        
+    }
+};
+
+// Question link -- https://leetcode.com/problems/add-two-integers/
