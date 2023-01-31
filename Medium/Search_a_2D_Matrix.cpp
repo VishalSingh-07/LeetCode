0c2c73a52735c8e6dbb066a329dba2ed6984f73f
VishalSingh-07 <vishalsinghagr25@gmail.com>
2023-01-31 16:35:42 +0530

    Adding Search a 2D Matrix Question

---
+++ Medium/Search_a_2D_Matrix.cpp
@@ -0,0 +1,123 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Brute Force Approach
+// Time complexity --> O(m*n) and Space --> O(1)
+// where m = number of columns and n= number of rows
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         for(int i=0;i<matrix.size();i++)
+//         {
+//             for(int j=0;j<matrix[i].size();j++)
+//             {
+//                 if(matrix[i][j]==target)
+//                 {
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+        
+//     }
+// };
+
+
+// Applying Binary Search on each row
+// Time complexity --> O(n+logm) and Space --> O(1)
+// where m = number of columns and n= number of rows
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         int n=matrix.size(); // Number of rows
+//         int m=matrix[0].size(); //Number of columns
+//         int low=0,high=m-1;
+//         while(low>=0 && low<n && high>=0 && high<m)
+//         {
+//             if(matrix[low][high]==target)
+//             {
+//                 return true;
+//             }
+//             else if(matrix[low][high]>target)
+//             {
+//                 high--;
+//             }
+//             else if(matrix[low][high]<target)
+//             {
+//                 low++;
+//             }
+//         }
+//         return false;
+        
+//     }
+// };
+
+// Start from last index
+// Time complexity --> O(m+n) and Space --> O(1)
+// where m = number of columns and n= number of rows
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         int n=matrix.size(); // Number of rows
+//         int m=matrix[0].size(); //Number of columns
+//         int i=0;
+//         int j=m-1;
+//         while(i<n && j>=0)
+//         {
+//             if(matrix[i][j]<target)
+//             {
+//                 i++;
+//             }
+//             else if(matrix[i][j]>target)
+//             {
+//                 j--;
+//             }
+//             else if(matrix[i][j]==target)
+//             {
+//                 return true;
+//             }
+//         }
+//         return false;
+        
+//     }
+// };
+
+
+// Optimized Approach
+// Time complexity --> O(log(m*n)) and Space --> O(1)
+// where m = number of columns and n= number of rows
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size(); // Number of rows
+        int m=matrix[0].size(); //Number of columns
+        int low=0;
+        int high=n*m-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(matrix[mid/m][mid%m]==target)
+            {
+                return true;
+            }
+            else if(matrix[mid/m][mid%m]>target)
+            {
+                high=mid-1;
+            }
+            if(matrix[mid/m][mid%m]<target)
+            {
+                low=mid+1;
+            }
+            
+        }
+        return false;
+        
+    }
+};
+
+
+// Question link -- https://leetcode.com/problems/search-a-2d-matrix/
+
+// Video Link -- https://youtu.be/ZYpYur0znng
+
+// My Detail Solution link -- https://leetcode.com/problems/search-a-2d-matrix/solutions/3122363/4-approach-to-solve-a-problem-easy-c-solution-brute-force-to-optimized-approach/
