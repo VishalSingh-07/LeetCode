3aa082f349c260d8f0dd4d757f80aa59d47194ea
VishalSingh-07 <vishalsinghagr25@gmail.com>
2023-07-31 21:16:48 +0530

    Updating Two Sum Question

--- Easy/Two_sum.cpp
+++ Easy/Two_sum.cpp
@@ -1,47 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 // Brute for approach
-/*
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        int n=nums.size();
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i+1;j<n;j++)
-            {
-                if(nums[i]+nums[j]==target)
-                {
-                    return {i,j};
-                }
-            }
-        }
-        return {-1,-1};
-    }
-};
-*/
+// Time complexity -> O(n^2) and Space -> O(1)
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         int n=nums.size();
+//         for(int i=0;i<n;i++)
+//         {
+//             for(int j=i+1;j<n;j++)
+//             {
+//                 if(nums[i]+nums[j]==target)
+//                 {
+//                     return {i,j};
+//                 }
+//             }
+//         }
+//         return {-1,-1};
+//     }
+// };
+
 
 // Hash table approach
+// Time complexity -> O(n) and Space -> O(1)
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans;
         unordered_map<int,int> mpp;
         for(int i=0;i<nums.size();i++)
         {
             if(mpp.find(target-nums[i])!=mpp.end())
             {
-                ans.push_back(mpp[target-nums[i]]);
-                ans.push_back(i);
-                return ans;
+                return {mpp[target-nums[i]],i};
             }
             mpp[nums[i]]=i;
 
         }
-        return ans;
+        return {-1,-1};
         
         
     }
 };
 
-// Question Link-- https://leetcode.com/problems/two-sum/
+/*
+
+1. Question link -- https://leetcode.com/problems/two-sum/
+
+2. Video Link -- https://youtu.be/UXDSeD9mN-k
+
+3. Solution link -- https://leetcode.com/problems/two-sum/solutions/3843569/2-approach-easy-c-solution-brute-force-and-optimized-approach/
+*/
