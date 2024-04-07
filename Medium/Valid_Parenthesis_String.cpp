2302c3d6cca708ffd33cf0922da8a6997de2216e
VishalSingh-07 <vishalsinghagr25@gmail.com>
2024-04-07 23:20:12 +0530

    Updating Valid Parenthesis String

--- Medium/Valid_Parenthesis_String.cpp
+++ Medium/Valid_Parenthesis_String.cpp
@@ -110,6 +110,33 @@ public:
     }
 };
 
+// OR
+
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int open=0; //For keeping count in Left to right travers
+        int close=0; //For keeping count in Right to left travers
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            if(s[i]=='(' || s[i]=='*'){
+                open++;
+            }else{
+                open--;
+            }
+            if(s[n-i-1]==')' || s[n-i-1]=='*'){
+                close++;
+            }else{
+                close--;
+            }
+
+            if(open<0 || close<0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
 
 
 
