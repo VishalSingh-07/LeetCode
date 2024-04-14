64cc5fb27a6b0e9b786bbf2c970eaf1448e0fd84
VishalSingh-07 <vishalsinghagr25@gmail.com>
2024-04-14 16:23:18 +0530

    Score of a String

---
+++ Easy/Score_of_a_String.cpp
@@ -0,0 +1,57 @@
+/*
+3110. Score of a String
+
+You are given a string s. The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters.
+
+Return the score of s.
+
+ 
+
+Example 1:
+
+Input: s = "hello"
+
+Output: 13
+
+Explanation:
+
+The ASCII values of the characters in s are: 'h' = 104, 'e' = 101, 'l' = 108, 'o' = 111. So, the score of s would be |104 - 101| + |101 - 108| + |108 - 108| + |108 - 111| = 3 + 7 + 0 + 3 = 13.
+
+Example 2:
+
+Input: s = "zaz"
+
+Output: 50
+
+Explanation:
+
+The ASCII values of the characters in s are: 'z' = 122, 'a' = 97. So, the score of s would be |122 - 97| + |97 - 122| = 25 + 25 = 50.
+
+ 
+
+Constraints:
+
+2 <= s.length <= 100
+s consists only of lowercase English letters.
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+// Optimized Approach
+// Time complexity -> O(n) and Space -> O(1)
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int sum=0;
+        for(int i=0;i<s.size()-1;i++){
+            sum+=abs(s[i]-s[i+1]);
+        }
+        return sum;
+    }
+};
+
+/*
+1. Question link -- https://leetcode.com/problems/score-of-a-string/
+
+2. Solution link -- https://leetcode.com/problems/score-of-a-string/solutions/5021809/beats-100-00-of-users-with-c-optimized-approach-best-c-solution-with-explanation
+*/
